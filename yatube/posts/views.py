43ddfd134744ca257e54897690fe21e211f8fd9a
Анастасия Долгова <a3894685@yandex.ru>
2022-07-29 13:15:23 +0300

    Подключение шаблона

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,9 +1,8 @@
 
 # Create your views here.
-from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse('Главная страница')
+    template = 'posts/index.html'
+    return render(request, template)
 
-def group_posts(request, posts):
-    return HttpResponse('Список друзей')
