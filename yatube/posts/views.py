0fd861d8125c6307aa4dbf6d21ca5d6953bc2702
Анастасия Долгова <a3894685@yandex.ru>
2022-08-02 16:50:39 +0300

    Пространство имен

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,12 +1,22 @@
 
 # Create your views here.
+from cgitb import text
+from turtle import title
+from typing import Text
 from django.shortcuts import render
 
 def index(request):
     templates = 'posts/index.html'
-    return render(request, templates)
+    title = 'Это главная страница проекта Yatube'
+    context = {
+        'title': title,
+    }
+    return render(request, templates, context)
 
 def group_posts(request, slug):
-    templates = 'posts/index.html'
-    return render(request, templates)
-
+    templates = 'posts/group_list.html'
+    text  = 'Здесь будет информация о группах проекта Yatube'
+    context = {
+        'text': text,
+    }
+    return render(request, templates, context)
