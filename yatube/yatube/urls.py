0fc3ac80ed64bacae35e74db7c277a09e2f0e398
Анастасия Долгова <a3894685@yandex.ru>
2022-08-03 21:59:40 +0300

    Создание суперпользователя

--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -20,7 +20,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', include('posts.urls', namespace='posts')),
-    path('group/<slug:slug>/', include('posts.urls')),
+    path('admin/', admin.site.urls),
 ]
